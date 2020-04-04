1eab1429b81734db713b4f51c14f8c6f3c491bc9
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-04-04 16:18:13 -0300

    Feature: creating global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import GlobalStyle from './styles/GlobalStyle';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
